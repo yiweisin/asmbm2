783e89e2c4d3d615f697ab22139e19132efd4737
yiweisin <88491304+yiweisin@users.noreply.github.com>
2025-04-16 08:51:34 +0800

    -

--- SocialMediaManager.API/obj/Debug/net9.0/SocialMediaManager.API.AssemblyInfo.cs
+++ SocialMediaManager.API/obj/Debug/net9.0/SocialMediaManager.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaManager.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e3c66720874dce8a5e09b25b71825127fef3bca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+199e6d2419c495a062629552bf4f1d711377cca4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaManager.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaManager.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
